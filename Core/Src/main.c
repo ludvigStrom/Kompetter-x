9f70976c6e15b40dc45bb613d3dae667619ed693
Ludvig Ström <ludvigstrom@gmail.com>
2023-07-20 18:15:38 +0200

    alpha smoothing

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,21 +1,4 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -73,7 +56,7 @@ typedef struct {
 
 mouseHID mousehid = {0x02,0,0,0,0,0};
 
-#define ALPHA_SMOOTHING 0.4
+#define ALPHA_SMOOTHING 0.2
 
 #define DEBOUNCE_DELAY 10 // Debounce delay in milliseconds
 
