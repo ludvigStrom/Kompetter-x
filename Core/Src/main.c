1a6463e9cc6c2a56cebab30da4096ab3786f69df
Ludvig Ström <ludvigstrom@gmail.com>
2023-07-21 10:30:27 +0200

    Refactor of keyboard scanning

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -35,17 +35,6 @@ typedef struct
 
 keyboardHID keyboardhid = {0x01,0,0,0,0,0,0,0,0};
 
-#define NUM_ROWS 4
-#define NUM_COLS 4
-#define NUM_KEYS 16
-
-uint8_t keycode_map[NUM_ROWS][NUM_COLS] = {
-    {KEY_1, KEY_2, KEY_3, KEY_4}, // HID keycodes for '1', '2', '3', '4'
-    {KEY_5, KEY_6, KEY_7, KEY_8}, // HID keycodes for '5', '6', '7', '8'
-    {KEY_9, KEY_A, KEY_B, KEY_C}, // HID keycodes for '9', 'A', 'B', 'C'
-    {KEY_D, KEY_E, KEY_F, KEY_G}  // HID keycodes for 'D', 'E', 'F', 'G'
-};
-
 typedef struct {
     uint8_t REPORT_ID;    		// REPORT_ID
     uint8_t BUTTONS;     		// Button states - bitmapped
@@ -58,14 +47,6 @@ typedef struct {
 mouseHID mousehid = {0x02,0,0,0,0,0};
 
 #define ALPHA_SMOOTHING 0.2
-
-#define DEBOUNCE_DELAY 10 // Debounce delay in milliseconds
-
-//Keyboard scanning
-uint32_t last_key_time[NUM_ROWS][NUM_COLS] = {0};
-uint8_t hid_report[NUM_KEYS] = {0};
-uint8_t hid_report_prev[NUM_KEYS] = {0};
-char last_key[3] = {0};
 
 //Magnetic encoder
 int16_t currentEncoderVal = 0;
@@ -103,8 +84,6 @@ I2C_HandleTypeDef hi2c2;
 SPI_HandleTypeDef hspi2;
 
 /* USER CODE BEGIN PV */
-char txBuf[8];
-uint8_t count = 1;
 
 /* USER CODE END PV */
 
@@ -211,15 +190,7 @@ int main(void)
   SSD1306_Puts("--  ", &Font_11x18, 1);
   SSD1306_UpdateScreen();
 
-  //debounce scan:
-  enum KeyState { IDLE, PRESSED };
-  enum KeyState key_state[NUM_ROWS][NUM_COLS];  // Declare the array of states
-
-  for(int i = 0; i < NUM_ROWS; i++) {
-    for(int j = 0; j < NUM_COLS; j++) {
-	  key_state[i][j] = IDLE;  // Initialize each state to IDLE
-	}
-  }
+  keyboardScannerInit();
 
   // Initialize lastEncoderVal with the initial encoder value
   lastEncoderVal = AS5600_ReadAngle(&hi2c2);
@@ -235,9 +206,6 @@ int main(void)
 
 
     /* USER CODE BEGIN 3 */
-
-	//TEST
-	keyboardScanner();
 
 	//ANGLE SENSOR
 	uint16_t angle = AS5600_ReadAngle(&hi2c2);
@@ -269,7 +237,7 @@ int main(void)
 
 		//ACCUMULATOR:
 	    //Convert to a string with leading spaces
-		sprintf(angle_str, "%5d", smoothedAccumulator);
+		sprintf(angle_str, "%5d", (int8_t)smoothedAccumulator);
 
 		//display accumulator
 		SSD1306_GotoXY (80,0);
@@ -282,48 +250,7 @@ int main(void)
 		SSD1306_Puts("No :(", &Font_11x18, 1);
 	}
 
-	//Keyboard Scan!
-	for(int i = 0; i < NUM_ROWS; i++) {
-	    uint32_t current_tick = HAL_GetTick();
-	    HAL_GPIO_WritePin(row_ports[i], row_pins[i], GPIO_PIN_SET);
-	    HAL_Delay(1); // delay after setting row high
-
-	    for(int j = 0; j < NUM_COLS; j++) {
-	        uint8_t is_pressed = HAL_GPIO_ReadPin(col_ports[j], col_pins[j]) == GPIO_PIN_SET;
-
-	        if (is_pressed && key_state[i][j] == IDLE) {
-	            // Key has been pressed from an idle state
-
-	            // Find a slot in the HID report
-	            for (int k = 0; k < NUM_KEYS; k++) {
-	                if (hid_report[k] == 0) { // 0 indicates an empty slot
-	                    hid_report[k] = keycode_map[i][j]; // Add the key to the HID report
-	                    break;
-	                }
-	            }
-	            key_state[i][j] = PRESSED;
-	            last_key_time[i][j] = current_tick;
-
-	            sprintf(last_key, "%d%d", i+1, j+1);  // Save the last key pressed
-
-	        } else if (!is_pressed && key_state[i][j] == PRESSED && current_tick - last_key_time[i][j] > DEBOUNCE_DELAY) {
-	            // Key has been released
-
-	            // Remove the key from the HID report
-	            for (int k = 0; k < NUM_KEYS; k++) {
-	                if (hid_report[k] == keycode_map[i][j]) {
-	                    hid_report[k] = 0; // Remove the key
-	                    break;
-	                }
-	            }
-
-	            key_state[i][j] = IDLE;
-	        }
-	    }
-
-	    HAL_GPIO_WritePin(row_ports[i], row_pins[i], GPIO_PIN_RESET);
-	    HAL_Delay(1); // delay after setting row low
-	}
+	keyboardScan();
 
 	// Check if the HID report has changed
 	uint8_t report_changed = 0;
